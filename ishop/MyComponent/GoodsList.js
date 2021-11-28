642eb053d9ced824f726c1aab887debb05cc24aa
Anastasiya <leshencko.nastia2013@gmail.com>
2021-11-28 19:40:20 +0300

    add table with goods list

--- ishop/MyComponent/GoodsList.js
+++ ishop/MyComponent/GoodsList.js
@@ -2,10 +2,45 @@ let GoodsList = React.createClass({
   displayName: "GoodsList",
 
   render: function () {
-    return React.DOM.div(
-      { className: "GoodsListFrame" },
-      React.DOM.h1(null, "Hello everyone"),
-      React.DOM.div({ className: "GoodsListText" }, "First Project")
+    let goodsList = [];
+
+    this.props.goodsList.forEach((good) => {
+      goodsList.push(
+        React.DOM.tr(
+          { key: good.inventoryNumber },
+          React.DOM.td(null, good.name),
+          React.DOM.td(null, good.price),
+          React.DOM.td(
+            null,
+            React.DOM.a({ href: good.url, target: "_blank" }, good.url)
+          ),
+          React.DOM.td(
+            null,
+            React.DOM.img({
+              className: "GoodsListPhoto",
+              src: good.photo,
+            })
+          ),
+          React.DOM.td(null, good.quantity)
+        )
+      );
+    });
+
+    return React.DOM.table(
+      { className: "GoodsListTable" },
+      React.DOM.caption(null, React.DOM.h2(null, this.props.shopName)),
+      React.DOM.thead(
+        null,
+        React.DOM.tr(
+          null,
+          React.DOM.th(null, "Good name"),
+          React.DOM.th(null, "Price"),
+          React.DOM.th(null, "Link to the good"),
+          React.DOM.th(null, "Photo"),
+          React.DOM.th(null, "Quantity")
+        )
+      ),
+      React.DOM.tbody(null, goodsList)
     );
   },
 });
