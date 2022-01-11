c009fb562b42c29df0205d8daae1f365ce1f7355
Anastasiya <leshencko.nastia2013@gmail.com>
2022-01-11 21:33:16 +0300

    read goods from json file

--- ishop3/src/index.js
+++ ishop3/src/index.js
@@ -3,30 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import Shop from "./Shop";
 import reportWebVitals from "./reportWebVitals";
-
-let goodsList = [
-  {
-    inventoryNumber: 1,
-    name: "IPhone 13 Pro",
-    price: "5 599 BYN",
-    url: "https://i-store.by/iphone/iphone-13-pro/mlwg3rka",
-    quantity: 10,
-  },
-  {
-    inventoryNumber: 2,
-    name: "Apple Watch 7",
-    price: "1 499 BYN",
-    url: "https://i-store.by/apple-watch/apple-watch-series-7/apple-watch-series-7-gps-41mm-temnaya-noch-sportivnyy-remeshok-cveta-temnaya-noch-mkmx3rba",
-    quantity: 15,
-  },
-  {
-    inventoryNumber: 3,
-    name: "AirPods 3",
-    price: "699 BYN",
-    url: "https://i-store.by/accessories/acoustics/apple-airpods/besprovodnye-naushniki-apple-airpods-mme73rua",
-    quantity: 20,
-  },
-];
+import goodsList from "./goodsList.json";
 
 ReactDOM.render(
   <React.StrictMode>
