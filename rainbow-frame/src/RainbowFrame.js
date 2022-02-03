9c73850f60000203f8d44a34e680d081ee3d79ff
Anastasiya <leshencko.nastia2013@gmail.com>
2022-02-03 22:13:18 +0300

    remove redundant div

--- rainbow-frame/src/RainbowFrame.js
+++ rainbow-frame/src/RainbowFrame.js
@@ -3,7 +3,7 @@ const RainbowFrame = (props) => {
   props.colors.forEach((color) => {
     code = <div style={{ border: `solid 5px ${color}` }}>{code}</div>;
   });
-  return <div>{code}</div>;
+  return code;
 };
 
 export default RainbowFrame;
